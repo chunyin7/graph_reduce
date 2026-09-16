251d3c9b298ba43bbdc478b5036aad2c422f9c29
miles <miles.c.y.ng@gmail.com>
2026-09-16 20:03:09 +1000

    basic rule

--- lib/graph.ml
+++ lib/graph.ml
@@ -3,7 +3,38 @@ open Node
 type graph = { nodes : node list; edges : (port * port) list }
 
 let find_node id nodes = List.find (fun node -> node.id = id) nodes
-let rule_a_b graph a b = graph
+
+let aux_id (port : port) =
+  match port.kind with Auxilary id -> Some id | Principal -> None
+
+let unwrap_aux_id (port : port) =
+  match port.kind with
+  | Auxilary id -> id
+  | Principal -> failwith "Expected auxilary port."
+
+let ( -- ) i j =
+  let rec aux n acc = if n < i then acc else aux (n - 1) (n :: acc) in
+  aux j []
+
+(* remove the nodes connect their auxilaries *)
+let rule_a_b graph a b =
+  let new_edges =
+    List.map
+      (fun id ->
+        ({ node = a; kind = Auxilary id }, { node = b; kind = Auxilary id }))
+      (0 -- 1)
+  in
+  let edges' =
+    List.filter
+      (fun ((p1, p2) : port * port) ->
+        p1.node != a && p2.node != a && p1.node != b && p2.node != b)
+      graph.edges
+    |> List.append new_edges
+  in
+  {
+    nodes = List.filter (fun n -> n.id != a && n.id != b) graph.nodes;
+    edges = edges';
+  }
 
 let step graph =
   match
@@ -12,7 +43,8 @@ let step graph =
         p1.kind = Principal && p2.kind = Principal)
       graph.edges
   with
-  | Some (p1, p2) -> (
+  | Some e -> (
+      let p1, p2 = e in
       let n1 = find_node p1.node graph.nodes in
       let n2 = find_node p2.node graph.nodes in
       match (n1.kind, n2.kind) with
